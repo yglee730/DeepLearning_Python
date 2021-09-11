f073fa29b1164256828fbe43a1a0b30667ef7f43
yglee730 <yglee730@naver.com>
2021-09-11 22:36:08 +0900

    넘파이로 N차원 배열 산술 계산

---
+++ useNumpy2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+A = np.array([[1,2], [3,4]])
+B = np.array([[3,0], [0,6]])
+
+print("======= A+B =======")
+print(A+B)
+print("======= A*B =======")
+print(A*B)
+print()
+
+X = np.array([[1,2], [3,4]])
+Y = np.array([10,20])
+
+print("======= X+Y =======")
+print(X+Y)
+print("======= X*Y =======")
+print(X*Y)
