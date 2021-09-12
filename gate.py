d4186432517b3346f8675cef0e6b73a02166728a
yglee730 <yglee730@naver.com>
2021-09-12 17:58:30 +0900

    numpy를 이용한 논리 회로 게이트 구현

---
+++ gate.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+'''
+ x1*w1 + x2*w2 > b
+ x1*w1 + x2*w2 - b > 0
+
+'''
+
+# AND GATE
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+    
+#NAND GATE
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1    
+
+#OR GATE
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+    
+#XOR GATE
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+print(XOR(1, 0))
+
+
